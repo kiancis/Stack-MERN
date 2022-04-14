4f091d1d7e568fe6a23869c8490b6da6f969f004
Kiancis Dominguez <kiancisd@gmail.com>
2022-04-14 09:55:31 -0400

    modifying HomePage

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,14 +1,18 @@
 import { usePosts } from "../Context/postContext";
 import { Link } from "react-router-dom";
 import { PostCard } from "../components/PostCard";
+import{VscEmptyWindow}from "react-icons/vsc";
 
 export function HomePage() {
   const { posts } = usePosts();
 
   const renderMain = () => {
-    if (posts.length === 0) {
-      return <div>We don't have posts</div>;
-    }
+    if (posts.length === 0) 
+      return <div className="flex flex-col justify-center items-center">
+      <VscEmptyWindow className="w-48 h-48 text-white"/>
+      <h1 className="text-2xl text-white">We don't have posts</h1>
+      </div>;
+     
     return (
       <div className="grid grid-cols-3 gap-4">
         {posts.map((post) => (
@@ -29,6 +33,7 @@ export function HomePage() {
           Create New Post
         </Link>
       </header>
+      {renderMain()}
     </div>
   );
 }
